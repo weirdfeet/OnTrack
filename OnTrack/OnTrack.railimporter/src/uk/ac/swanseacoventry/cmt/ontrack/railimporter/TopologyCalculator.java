9a7e117efca57b9ac089cccaa8cceb4dcb1ce6b4
Hoang Nga Nguyen <hoangnga@gmail.com>
2017-06-29 14:46:46 +0100

    - add imports

--- OnTrack/OnTrack.railimporter/src/uk/ac/swanseacoventry/cmt/ontrack/railimporter/TopologyCalculator.java
+++ OnTrack/OnTrack.railimporter/src/uk/ac/swanseacoventry/cmt/ontrack/railimporter/TopologyCalculator.java
@@ -9,7 +9,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Scanner;
 
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -17,6 +20,7 @@ import org.eclipse.emf.ecore.xmi.impl.XMIResourceFactoryImpl;
 
 import uk.ac.swanseacoventry.cmt.ontrack.Connector;
 import uk.ac.swanseacoventry.cmt.ontrack.ControlTableItem;
+import uk.ac.swanseacoventry.cmt.ontrack.DirectedTrack;
 import uk.ac.swanseacoventry.cmt.ontrack.OntrackFactory;
 import uk.ac.swanseacoventry.cmt.ontrack.Track;
 import uk.ac.swanseacoventry.cmt.ontrack.TrackPlan;
